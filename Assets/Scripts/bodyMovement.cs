065af1864fd1b81069719912e31fc73317ea7802
Dave <davy.bolivar@gmail.com>
2014-12-06 18:19:45 +0800

    Added Players Foot and Animation

---
+++ Assets/Scripts/bodyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bodyMovement : MonoBehaviour {
+	public GameObject leftLeg;
+	public GameObject rightLeg;
+	public GameObject body;
+	public float legDur, legMax, legMin;
+	public float bodyDur, bodyMax, bodyMin;
+	public GameObject moveScript;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//IF PLAYER IS MOVING
+		if(moveScript.GetComponent<PlayerControl>().move.x != 0 || moveScript.GetComponent<PlayerControl>().move.y != 0)
+		{   
+
+			//LEG
+			leftLeg.transform.rotation = rightLeg.transform.rotation = Quaternion.Euler(leftLeg.transform.localRotation.x, leftLeg.transform.localRotation.y,
+			                                                                            Mathf.PingPong(Time.time * legDur, legMax-legMin)+legMin);
+			//BODY
+			body.transform.localPosition = new Vector3(body.transform.localPosition.x, Mathf.PingPong(Time.time * bodyDur, bodyMax-bodyMin)+bodyMin, body.transform.localPosition.z);
+		}
+		else
+		{
+			//LEG
+			leftLeg.transform.rotation = rightLeg.transform.rotation = transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(
+																						leftLeg.transform.localRotation.x,leftLeg.transform.localRotation.y,0f)),1f);
+			//BODY
+			body.transform.localPosition = Vector3.Lerp(body.transform.localPosition,new Vector3(body.transform.localPosition.x,0f,body.transform.localPosition.z),1f);
+		}
+	}
+}
