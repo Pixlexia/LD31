554750f5216cbfa195b9e651249f77557c5844c5
Alfonz Montelibano <m.alfonz@gmail.com>
2014-12-07 00:12:58 +0800

    added keys, effects, polish

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -22,11 +22,22 @@ public class FollowPlayer : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Vector3.Distance (transform.position, target.transform.position) > 1f && Vector3.Distance (transform.position, target.transform.position) < 10){
+		if (Vector3.Distance (transform.position, target.transform.position) > 1.5f && Vector3.Distance (transform.position, target.transform.position) < 10){
 			transform.position = Vector3.Lerp (transform.position, target.transform.position, 0.05f);
 			isMoving = true;
 		}
+		else{
+			isMoving = false;
+		}
 
 		rigidbody2D.velocity = Vector2.Lerp (rigidbody2D.velocity, Vector2.zero, 0.2f);
+	}
+
+	void OnEnable(){
+		isMoving = false;
+	}
+
+	void OnDisable(){
+		isMoving = false;
 	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public ColoredPath.MyColor colortype;
+
+	// Particles
+	public GameObject dieParticle1, playerColorParticle, keyParticle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Die(){
+		if (dieParticle1 && playerColorParticle) {
+			Instantiate (dieParticle1, transform.position + new Vector3(0,0.3f,0), Quaternion.identity);
+			GameObject p = Instantiate (playerColorParticle, transform.position + new Vector3(0,0.3f,0), Quaternion.identity) as GameObject;
+//			Debug.Log (transform.GetChild(0).GetComponent<SpriteRenderer>().color);
+//			Debug.Log (p);
+			p.GetComponent<ParticleSystem>().startColor = transform.GetChild(0).GetComponent<SpriteRenderer>().color;
+		}
+
+		gameObject.SetActive (false);
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Deadly") {
+			Die ();		
+		}
+		else if(col.gameObject.tag == "Key"){
+			Destroy (col.gameObject);
+			Instantiate(keyParticle, transform.position, Quaternion.identity);
+		}
+	}
+}
