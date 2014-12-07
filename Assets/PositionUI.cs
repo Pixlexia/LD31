4ddc57f9632f7aae7e1d416e792007daa42c77f5
Dave <davy.bolivar@gmail.com>
2014-12-07 23:26:31 +0800

    Added position of text

--- Assets/PositionUI.cs
+++ Assets/PositionUI.cs
@@ -2,28 +2,17 @@
 using System.Collections;
 
 public class PositionUI : MonoBehaviour {
-	public int maxHealth=100; 
-	public GameObject target;
-	public int curHealth=50; 
-	public Vector3 screenPosition;
-	public float healthBarLenght;
 
+	public GameObject player; 
+	public float yoffset;
 	// Use this for initialization
 	void Start () { 
-		healthBarLenght=Screen.width/2; 
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnGUI(){
-		
-		screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-		screenPosition.y = Screen.height - screenPosition.y;
-		
-		GUI.Box(new Rect(screenPosition.x-10,screenPosition.y-40,healthBarLenght,20),curHealth+"/"+maxHealth);
+	void FixedUpdate () {
+		gameObject.transform.position = new Vector3 (player.transform.position.x,player.transform.position.y + yoffset,0);
 	}
 
 }
