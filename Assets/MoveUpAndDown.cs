66977965f52267fbed40c394e01658b160bc9110
Dave <davy.bolivar@gmail.com>
2014-12-07 10:54:56 +0800

    Added Better Platforms

---
+++ Assets/MoveUpAndDown.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUpAndDown : MonoBehaviour {
+	public bool floatup;
+	public Vector3 temp,temp2;
+	public float range,hovertime;
+	// Use this for initialization
+	void Start () {
+		temp = transform.position;
+		temp2 = transform.position;
+		floatup = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(floatup)
+			StartCoroutine(floatingup());
+		else if(!floatup)
+			StartCoroutine(floatingdown());
+	}
+	
+	public IEnumerator floatingup(){
+		temp.y = temp.y + range * Time.deltaTime;
+		transform.position = temp;
+		yield return new WaitForSeconds(hovertime);
+		floatup = false;
+	}
+	public IEnumerator floatingdown(){
+		temp.y -= range * Time.deltaTime;
+		transform.position = temp;
+		yield return new WaitForSeconds(hovertime);
+		floatup = true;
+	}
+}
