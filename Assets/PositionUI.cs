4f6f6d764c04085096f134f444ced2559460f3a5
Dave <davy.bolivar@gmail.com>
2014-12-07 22:30:53 +0800

    Improved Text Animation

---
+++ Assets/PositionUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionUI : MonoBehaviour {
+	public int maxHealth=100; 
+	public GameObject target;
+	public int curHealth=50; 
+	public Vector3 screenPosition;
+	public float healthBarLenght;
+
+	// Use this for initialization
+	void Start () { 
+		healthBarLenght=Screen.width/2; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		
+		screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		screenPosition.y = Screen.height - screenPosition.y;
+		
+		GUI.Box(new Rect(screenPosition.x-10,screenPosition.y-40,healthBarLenght,20),curHealth+"/"+maxHealth);
+	}
+
+}
