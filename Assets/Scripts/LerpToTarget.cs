0cf91f3f955b8380d19330e25a20718b1fa28959
Alfonz Montelibano <m.alfonz@gmail.com>
2014-12-09 08:53:21 +0800

    added FINAL scene

--- Assets/Scripts/LerpToTarget.cs
+++ Assets/Scripts/LerpToTarget.cs
@@ -5,8 +5,11 @@ public class LerpToTarget : MonoBehaviour {
 
 	public GameObject target;
 	public float lerpamt;
+
+	public static bool start = true;
 	
 	void FixedUpdate () {
+
 		transform.position = Vector3.Lerp (transform.position, new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z), lerpamt);
 	}
 }
---
+++ Assets/StartingCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartingCamera : MonoBehaviour {
+
+	public GameObject p;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp (transform.position, p.transform.position, 0.005f);
+
+		if(Vector3.Distance(p.transform.position, transform.position) < 0.1f){
+			p.GetComponent<PlayerControl>().enabled = true;
+			p.GetComponent<LerpToTarget>().enabled = true;
+			GetComponent<StartingCamera>().enabled = false;
+		}
+	}
+}
